5e14cd2b8f9aafc58583e00dbe346888fb66b046
Tegar Oakley <tegaroakley@gmail.com>
2021-06-03 16:24:55 +0700

    baru splash dan walkthrough

---
+++ app/src/main/java/com/example/myapssbuatuastakepertama/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.myapssbuatuastakepertama;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 3500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent home = new Intent(MainActivity.this, Intro.class);
+                startActivity(home);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
