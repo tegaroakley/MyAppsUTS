e3f6812fdb027d8bbae2dedd134bd049d94b8bf0
Tegar Oakley <tegaroakley@gmail.com>
2021-06-04 11:28:09 +0700

    bot nav

--- app/src/main/java/com/example/myapssbuatuastakepertama/Home.java
+++ app/src/main/java/com/example/myapssbuatuastakepertama/Home.java
@@ -1,8 +1,14 @@
 package com.example.myapssbuatuastakepertama;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +16,11 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this,  R.id.fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+
     }
 }
